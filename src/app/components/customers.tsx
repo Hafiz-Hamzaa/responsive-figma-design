58fc72cb62f9c0eb5b135e3bbb186751232994c2
Hafiz Hamza <161071471+Hafiz-Hamzaa@users.noreply.github.com>
2024-11-18 06:21:17 -0800

    Update customers.tsx

--- src/app/components/customers.tsx
+++ src/app/components/customers.tsx
@@ -10,9 +10,9 @@ export default function Customers() {
             {/* customers */}
             <h1 className="font-black text-[42px] ml-40 mt-10 max-sm:relative right-20 max-sm:text-[32px]">OUR HAPPY CUSTOMERS</h1>
             <div className="flex gap-2 mt-10 ml-40 flex-wrap max-sm:relative right-20 bottom-6 ">
-                <Image src={custom} alt="img" width={410} height={500} className="max-sm:w-[230px]  max-sm:filter max-sm:brightness-0 max-sm:bg-white"/>
-                <Image src={custom1} alt="img" width={410} height={500}  className="max-sm:w-[230px]  max-sm:filter max-sm:brightness-0 max-sm:bg-white"/>
-                <Image src={custom2} alt="img" width={410} height={500}  className="max-sm:w-[230px]  max-sm:filter max-sm:brightness-0 max-sm:bg-white"/>
+                <Image src={custom} alt="img" width={410} height={500} className="max-sm:w-[230px]   max-sm:bg-white"/>
+                <Image src={custom1} alt="img" width={410} height={500}  className="max-sm:w-[230px]  max-sm:bg-white"/>
+                <Image src={custom2} alt="img" width={410} height={500}  className="max-sm:w-[230px]   max-sm:bg-white"/>
             </div>
             <Image src={footer} alt="footer" className="mt-6"/>
         </div>
